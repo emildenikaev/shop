fa3f6b938d5005e54966a0a23307c0de04c63727
Denikaev Emil <edenikaev@mail.ru>
2023-05-16 18:56:57 +0400

    add data

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,20 +1,52 @@
 <template>
   <div class="v-catalog-item">
-    <p>Item 1</p>
-    <p>Price: 100</p>
-    <button>Add to cart</button>
+    <img
+      :src="require('../assets/images/' + product_data.image)"
+      alt="Купить кофту в МСК"
+      class="v-catalog-item__image"
+    />
+    <p class="v-catalog-item__name">{{ product_data.name }}</p>
+    <p class="v-catalog-item__price">{{ product_data.price.toFixed(2) }} р</p>
+    <button
+      @click="sendDataToParent"
+      class="v-catalog-item__add_to_cart_btn btn"
+    >
+      Add to cart
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   name: "v-catalog-item",
-  props: {},
+  props: {
+    product_data: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
   data() {
     return {};
   },
   computed: {},
+  methods: {
+    sendDataToParent() {
+      this.$emit("sendArticle", this.product_data.article);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.v-catalog-item {
+  flex-basis: 25%;
+  box-shadow: 0 0 8px 0 #e0e0e0;
+  margin: 16px;
+  padding: 16px;
+  &__image {
+    width: 100px;
+  }
+}
+</style>
--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="v-catalog">
     <h1>Catalog</h1>
-    <vCatalogItem />
+    <div class="v-catalog__list">
+      <vCatalogItem
+        v-for="product in products"
+        :key="product.article"
+        :product_data="product"
+        @sendArticle="showChildArticle"
+      />
+    </div>
   </div>
 </template>
 
@@ -15,10 +22,69 @@ export default {
   },
   props: {},
   data() {
-    return {};
+    return {
+      products: [
+        {
+          image: "1.jpg",
+          name: "T-shirt 1",
+          price: 2100.234234234,
+          article: "T1",
+          available: true,
+        },
+        {
+          image: "2.jpg",
+          name: "T-shirt 2",
+          price: 3150.12312412,
+          article: "T2",
+          available: true,
+        },
+        {
+          image: "3.jpg",
+          name: "T-shirt 3",
+          price: 4200.51524,
+          article: "T3",
+          available: false,
+        },
+        {
+          image: "4.jpg",
+          name: "T-shirt 4",
+          price: 5300.1245512,
+          article: "T4",
+          available: true,
+        },
+        {
+          image: "5.jpg",
+          name: "T-shirt 5",
+          price: 6500.3522314,
+          article: "T5",
+          available: false,
+        },
+        {
+          image: "6.jpeg",
+          name: "T-shirt 6",
+          price: 8700.4124123,
+          article: "T6",
+          available: true,
+        },
+      ],
+    };
   },
   computed: {},
+  methods: {
+    showChildArticle(data) {
+      console.log(data);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.v-catalog {
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
